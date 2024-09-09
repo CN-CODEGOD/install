88f01d3fcdbe2b550e4e82713b2fcde6658bf86b
cn_codegod <qq1792926897@gmail.com>
2024-09-09 18:46:23 +0800

    [9/9]删除FFMEPG

--- install_software.ps1
+++ install_software.ps1
@@ -36,25 +36,22 @@ md $home\software
 aria2c.exe "https://github.com/PowerShell/PowerShell/releases/download/v7.4.5/PowerShell-7.4.5-win-x64.msi" -d $home\software
 git clone https://github.com/marhau-dev/Soundpad-cracked.git
 aria2c.exe "https://github.com/medokin/soundpad-text-to-speech/releases/download/v1.1.1/Setup.exe" -d $home\software
-aria2c.exe "https://cdn-fastly.obsproject.com/downloads/OBS-Studio-30.2.3-Windows-Installer.exe"
-aria2c.exe "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z"
+aria2c.exe "https://cdn-fastly.obsproject.com/downloads/OBS-Studio-30.2.3-Windows-Installer.exe" -d $home\software
+aria2c.exe "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z"-d $home\software
+
 winget install Microsoft.PowerToys -s winget
 
 winget install JanDeDobbeleer.OhMyPosh -s winget
 aria2c.exe "https://nodejs.org/dist/v20.17.0/node-v20.17.0-x64.msi"
+#compress file 
+Rename-Item C:\Users\Administrator\git\ffmpeg-git-essentials.7z ffmepg.zip
+Compress-Archive $home\git\ffmpeg.zip 
+
 #set env 
-get-ffmepg
-function get-ffmepg {
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory)]
-    [string]
-    $ffmepg
-)
-    $Global:ffmepg=$ffmepg
-}
+
+
 $oldpath=[System.Environment]::GetEnvironmentVariable('path')
-$newpath=$oldpath+";$global:aria2c"
+$newpath=$oldpath+";$home\install\ffmepg\bin"
 [System.Environment]::SetEnvironmentVariable('path',$newpath,'machine')
 
 Restart-Computer
