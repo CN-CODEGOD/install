2359499f90ecee1581960a539fb0a9e6cad6c8c2
cn_codegod <qq1792926897@gmail.com>
2024-09-10 17:58:24 +0800

    [9/10]添加install bitcombet.vmware.

--- install_software.ps1
+++ install_software.ps1
@@ -1,5 +1,5 @@
 
-function instal_software {
+function install_software {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -57,6 +57,9 @@ function instal_software {
     aria2c.exe "https://nodejs.org/dist/v20.17.0/node-v20.17.0-x64.msi" -d $path\software
     aria2c.exe "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z" -d $path\software
     aria2c.exe "https://d1vdn3r1396bak.cloudfront.net/installer/363949053574335076/0588175" -d $path\software
+    aria2c.exe "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe" -d $path\software
+    aria2c.exe "https://downloads2.broadcom.com/?file=VMware-workstation-full-17.6.0-24238078.exe&oid=31617506&id=QsFTDf239JaXNuFAVwhhnA2nsRKZg85h2pvvA6dzJmEmfeD-OCQL_wPB-SzMANRgY8U=&verify=1725952613-YZaC5tPc5dCbUxlHHKNTSSmPTH6p1amyrR8JahVpEcU%3D" -d $path\software
+    aria2c.exe "https://d3mb6vraut285i.cloudfront.net/installer/0973117252055536153/437276" -d $path\software
     # move file 
     move $home\install\yt-dlp $path\software
   
@@ -75,4 +78,4 @@ function instal_software {
     
     Restart-Computer
 }
-instal_software
+install_software
