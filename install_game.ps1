955276607b9826e11bdb3b36fa94952603bd5955
cn_codegod <qq1792926897@gmail.com>
2024-09-10 20:27:54 +0800

    [9/11]添加install r星,EPIC games

--- install_game.ps1
+++ install_game.ps1
@@ -29,9 +29,11 @@ function get-aria2c {
 
 }
     md $home\game
-    aria2c.exe "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe" -d $home\game
-    aria2c.exe "https://dldir1.qq.com/tgc/wegame/miniloader/WeGameMiniLoader.std.5.12.21.1022.exe" -d $home\game
-    aria2c.exe "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.46.1/xmcl-0.46.1-win32-x64.zip" -d $home\game
+    aria2c.exe "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe" -d $path\game
+    aria2c.exe "https://dldir1.qq.com/tgc/wegame/miniloader/WeGameMiniLoader.std.5.12.21.1022.exe" -d $path\game
+    aria2c.exe "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.46.1/xmcl-0.46.1-win32-x64.zip" -d $path\game
+    aria2c.exe "https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe?_gl=1*1uzr7ch*_ga*MjEwMTA5MDc2OC4xNzI1OTcwMjYx*_ga_PJQ2JYZDQC*MTcyNTk3MDI2MC4xLjAuMTcyNTk3MDI2NC4wLjAuMA.." -d $path\game
+    aria2c.exe "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Win32/EpicInstaller-15.17.1.msi?launcherfilename=EpicInstaller-15.17.1-924c3ca127ce42be9c3ae18789d2cd86.msi" -d $path\hame
     
     
 }
