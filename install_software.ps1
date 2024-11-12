c00bd44bed8138bcac034ba3981fd76ee12821fd
CN_codegod <qq1792926897@gmail.com>
2024-11-13 06:43:22 +0800

    [11/13,hello cn_codegod,用curl随便做了 ,大部分的软件都可以用scoop.wget,winget下载.不能的用curl,磁力用aria2 ]

--- install_software.ps1
+++ install_software.ps1
@@ -19,82 +19,81 @@ function install_software {
     catch {
         instal_software
     }
-    try {
-        aria2c.exe
+
+ 
     
-    }
-    catch {
-        get-aria2c
-    }
-    try {
-   
-    }
-    catch {
-        throw 
-    }
-        function get-aria2c {
-            [CmdletBinding()]
-            param (
-                [Parameter(Mandatory)]
-                [string]
-                $aria2c
-            )
-            try {
-                & $aria2c        
-            }
-            catch {
-                write 
-            install_software
-            }
-        
-        }
+function get-wget {
+ #scooptintaller
+curl "https://github.com/ScoopInstaller/Install/blob/master/install.ps1" --output .\install.1.ps1
 
-
-aria2c.exe "https://github.com/ScoopInstaller/Install/blob/master/install.ps1"
-
-        irm get.scoop.sh -outfile 'install.1.ps1'
-.\install.ps1 -RunAsAdmin 
+irm get.scoop.sh -outfile 'install.1.ps1'
+.\install.1.ps1 -RunAsAdmin 
 # I don't care about other parameters and want a one-line command
 iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 
+scoop install wget
+}
 
     md $path\software
         cd software 
-    git clone https://github.com/marhau-dev/Soundpad-cracked.git 
+    #soundpad
+        git clone https://github.com/marhau-dev/Soundpad-cracked.git 
+        try {
+            wget
+        }
+        catch {
+            get_wget
+        }
+        #
+    #winget
     wget winget 
+    #ohmyposh
     winget install JanDeDobbeleer.OhMyPosh -s winget
+    #pwsh
     winget install --id Microsoft.PowerShell.Preview --source winget
-    aria2c.exe "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe" -d $path\software
-    aria2c.exe "https://github.com/medokin/soundpad-text-to-speech/releases/download/v1.1.1/Setup.exe" -d $path\software
-    aria2c.exe "https://cdn-fastly.obsproject.com/downloads/OBS-Studio-30.2.3-Windows-Installer.exe" -d $path\software
-    aria2c.exe "https://github.com/yt-dlp/yt-dlp/releases/download/2024.08.06/yt-dlp.exe" -d $path\software\yt-dlp
-    aria2c.exe "https://nodejs.org/dist/v20.17.0/node-v20.17.0-x64.msi" -d $path\software
-    aria2c.exe "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z" -d $path\software
-    aria2c.exe "https://d1vdn3r1396bak.cloudfront.net/installer/363949053574335076/0588175" -d $path\software
-    aria2c.exe "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe" -d $path\software
-    aria2c.exe "https://downloads2.broadcom.com/?file=VMware-workstation-full-17.6.0-24238078.exe&oid=31617506&id=QsFTDf239JaXNuFAVwhhnA2nsRKZg85h2pvvA6dzJmEmfeD-OCQL_wPB-SzMANRgY8U=&verify=1725952613-YZaC5tPc5dCbUxlHHKNTSSmPTH6p1amyrR8JahVpEcU%3D" -d $path\software
-    aria2c.exe "https://d3mb6vraut285i.cloudfront.net/installer/0973117252055536153/437276" -d $path\software
-    aria2c.exe "https://www.zhornsoftware.co.uk/stickies/stickies_setup_10_1d.exe" -d $path\software
-    aria2c.exe "https://lddl01.ldmnq.com/downloader/ldplayerinst9.exe?n=ldplayer9_ld_999_ld.exe" -d $path\software
-    aria2c.exe "https://github.com/NickeManarin/ScreenToGif/releases/download/2.41.1/ScreenToGif.2.41.1.Setup.x64.msi" -d $path\software
-    aria2c.exe "https://7-zip.org/a/7z2408-x64.exe"   -d $path\software
-    aria2c.exe "https://github.com/Xizo-114514/DiskIconChangerByXizo/releases/download/v1.2.2024.7.17/XizoDiskIconChangerV1.2.exe" -d $path\software
+    #powertoys
+    winget install Microsoft.PowerToys -s winget
+    #lghub
+    Start-Job {curl "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe" --output $Path\software\lghub_installer.exe}
+    #soundpad-text-to-speech
+    start-job {curl "https://github.com/medokin/soundpad-text-to-speech/releases/download/v1.1.1/Setup.exe" --output $path\software\soundpad-text-to-speech.exe}
+    #obs
+    start-job{ curl "https://cdn-fastly.obsproject.com/downloads/OBS-Studio-30.2.3-Windows-Installer.exe" --output $path\software\obs_installer.exe }
+    #yt-dlp
+    md $Path\soft\yt-dlp
+    start-job {curl "https://github.com/yt-dlp/yt-dlp/releases/download/2024.08.06/yt-dlp.exe" --output $path\software\yt-dlp\yt-dlp.exe}
+ #node 
+ scoop install node 
+ #python 
+ scoop install python
+    #FFMPEG
+    md $path\software\ffmpeg
+    start-job {curl "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z" --output $path\software\ffmpeg\ffmpeg.exe}
+    
+    start-job {curl "https://d1vdn3r1396bak.cloudfront.net/installer/363949053574335076/0588175" --output $path\software\cloudfromt}
+    #lghub
+    start-job {curl "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe" --output $path\software\lghub_installer.exe}
+    
+    start-job {curl "https://d3mb6vraut285i.cloudfront.net/installer/0973117252055536153/437276" --output $path\software\cloudfront_installer.exe}
+    start-job {curl "https://www.zhornsoftware.co.uk/stickies/stickies_setup_10_1d.exe" --output $path\software\stickies_installer.exe}
+    start-job {curl "https://lddl01.ldmnq.com/downloader/ldplayerinst9.exe?n=ldplayer9_ld_999_ld.exe" --output $path\software\ldplayer_installer.exe}
+    start-job {curl "https://github.com/NickeManarin/ScreenToGif/releases/download/2.41.1/ScreenToGif.2.41.1.Setup.x64.msi" --output $path\software\ScreenToGif_installer.exe}
+    start-job {curl "https://7-zip.org/a/7z2408-x64.exe"   --output $path\software\7z_setup.exe}
+    start-job{ curl "https://github.com/Xizo-114514/DiskIconChangerByXizo/releases/download/v1.2.2024.7.17/XizoDiskIconChangerV1.2.exe" --output $path\software\DiskIconChangerByXizo_installer.exe}
+    start-job {curl "https://download.mypikpak.net/desktop/official_PikPak.exe" --output $Path\software\pikpak_installer.exe}
     git clone https://github.com/CN-CODEGOD/icons.git 
     
-    # move file 
-    move $home\install\yt-dlp $path\software
+    
+    
   
     
-    #compress file 
+    
 
     
-    #set env 
     
     
-    $oldpath=[System.Environment]::GetEnvironmentVariable('path')
-    $newpath= ";$path\software\soundpad-craked\soundpad\"  
+ 
     
-    [System.Environment]::SetEnvironmentVariable('path',$newpath,'machine')
     
     Restart-Computer
 }
