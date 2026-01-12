0eca8c71c4f116ff28fa79dbc69eb898a78586ac
CN_codegod <qq1792926897@gmail.com>
2026-01-12 19:23:27 +0800

    delete yt-dlp etc unused files

--- config_env.ps1
+++
@@ -1,9 +0,0 @@
-
-
-
-$oldpath= [System.Environment]::GetEnvironmentVariable('path')
-$newpath = $oldpath+ ";$home\install\aria2-1.37.0-win-32bit-build1"+ ";$home\install\BBDown_1.6.3_20240814_win-x64" +";$home\install\yt-dlp"
-[System.Environment]::SetEnvironmentVariable('path',$newpath,'machine')
-$psmodulepath = [System.Environment]::GetEnvironmentVariable("psmodulepath")
-[System.Environment]::SetEnvironmentVariable('psmodulepath',$psmodulepath + ";$PSScriptRoot\git",'machine')
-Restart-Computer        
